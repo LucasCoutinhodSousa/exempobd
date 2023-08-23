8e13f41139c9958159fb6ac9dcc5593aff8f1601
LucasCoutinhodSousa <124942844+LucasCoutinhodSousa@users.noreply.github.com>
2023-08-22 21:17:48 -0300

    link

--- php/tabela_produto.php
+++ php/tabela_produto.php
@@ -15,36 +15,40 @@
     <head>
         <title>Tabela Produto</title>
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <link rel="stylesheet" href="../style/estilo.css">
     </head>
     <body>
-        <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Nome</th>
-                    <th>Valor</th>
-                    <th>Quantidade</th>
-                </tr>
+        <div class="principal">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Valor</th>
+                        <th>Quantidade</th>
+                    </tr>
 
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Maca</td>
-                    <td>8,99</td>
-                    <td>28</td>
-                </tr>
-                <?php
-                    foreach($produtos as $produtos){
-                        echo '<tr>';
-                        echo '<td>'.$produtos['id'].'</td>';
-                        echo '<td>'.$produtos['nome'].'</td>';
-                        echo '<td>'.$produtos['valor'].'</td>';
-                        echo '<td>'.$produtos['quantidade'].'</td>';
-                        echo '</tr>';
-                    }
-                ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Maca</td>
+                        <td>8,99</td>
+                        <td>28</td>
+                    </tr>
+                    <?php
+                        foreach($produtos as $produtos){
+                            echo '<tr>';
+                            echo '<td>'.$produtos['id'].'</td>';
+                            echo '<td>'.$produtos['nome'].'</td>';
+                            echo '<td>'.$produtos['valor'].'</td>';
+                            echo '<td>'.$produtos['quantidade'].'</td>';
+                            echo '<td><a href=edit_produto.php>'.'Editi</a></td>';
+                            echo '</tr>';
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>

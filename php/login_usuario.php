d79dda5b57662ccbf5a8f98f95550b36f89ad465
LucasCoutinhodSousa <124942844+LucasCoutinhodSousa@users.noreply.github.com>
2023-09-28 19:40:52 -0300

    login sucesso

--- php/login_usuario.php
+++ php/login_usuario.php
@@ -1,6 +1,25 @@
 <?php
     include('conexao.php');
     
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+
     $login = $_POST['login'];
-    
+    $senha = md5($_POST['senha']);
+
+    $query = $dbh->prepare('SELECT login, senha FROM login
+    WHERE login=:login AND senha=:senha');
+
+    $query->execute(array(
+        ':login' => $login,
+        ':senha' => $senha
+    ));
+
+    $usuario = $query->fetch();
+    if(isset($usuario['login'])){
+        echo 'sucesso';
+    }else{
+        echo 'Falha login';
+    }
 ?>
